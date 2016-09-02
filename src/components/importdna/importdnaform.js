690f7ac9b6308efc738c8f94c6e423a63328528d
Duncan Meech <duncanmeech@gmail.com>
2016-09-02 14:51:55 -0700

    start experiments for new sequence editor widget

--- src/components/importdna/importdnaform.js
+++ src/components/importdna/importdnaform.js
@@ -22,6 +22,7 @@ import { uiSetGrunt } from '../../actions/ui';
 import ModalWindow from '../modal/modalwindow';
 import { blockCreate, blockAddComponent } from '../../actions/blocks';
 import { dnaLoose, dnaLooseRegexp } from '../../utils/dna/dna';
+import SequenceEditor from './sequence-editor';
 
 import '../../../src/styles/form.css';
 import '../../../src/styles/importdnaform.css';
@@ -44,10 +45,9 @@ class DNAImportForm extends Component {
   constructor() {
     super();
     this.state = {
-      inputValid: true,
-      validLength: 0,
       // we don't want to render until we have the sequence
       haveSequence: false,
+      sequence: '',
     };
   }
 
@@ -71,18 +71,15 @@ class DNAImportForm extends Component {
         return;
       }
       // can edit the sequence of blocks that were previously manually edited or have no source
-      if (!(block.source.source === '' || block.source.source === 'user')) {
-        this.props.uiShowDNAImport(false);
-        this.props.uiSetGrunt(`You cannot add sequence to a block obtained from ${block.source.source}`);
-        return;
-      }
+      // if (!(block.source.source === '' || block.source.source === 'user')) {
+      //   this.props.uiShowDNAImport(false);
+      //   this.props.uiSetGrunt(`You cannot add sequence to a block obtained from ${block.source.source}`);
+      //   return;
+      // }
       block.getSequence()
         .then(sequence => {
           this.setState({
-            inputValid: true,
-            validLength: sequence ? sequence.length : 0,
             sequence: sequence || '',
-            haveSequence: true,
           });
         })
         .catch(() => {
@@ -93,94 +90,52 @@ class DNAImportForm extends Component {
     }
   }
 
-  onSequenceChanged(evt) {
-    const source = evt.target.value;
-    if (source) {
-      // strip anything except atgc and whitespace
-      const cleanRegex = new RegExp(`[^${dnaLoose}]`, 'gmi');
-      const clean = source.replace(cleanRegex, '');
-      if (clean !== source) {
-        evt.target.value = clean;
-      }
-      // check for valid sequence
-      // ( you should not be able to enter an invalid sequence but just in case )
-      const dnaRegex = dnaLooseRegexp();
-      const isValid = dnaRegex.test(clean);
-      this.setState({
-        inputValid: isValid,
-        validLength: clean.length,
-        sequence: clean,
-      });
-    } else {
-      this.setState({
-        inputValid: true,
-        validLength: 0,
-        sequence: null,
-      });
-    }
-  }
 
   onSubmit(evt) {
     evt.preventDefault();
-    // be sure we have a valid sequence before continuing
-    if (this.state.inputValid
-      && this.state.validLength
-      && this.state.sequence
-      && this.state.validLength === this.state.sequence.length) {
-      this.setSequenceAndClose(this.props.focusedBlocks[0], this.state.sequence);
-    }
+    // // be sure we have a valid sequence before continuing
+    // if (this.state.inputValid
+    //   && this.state.validLength
+    //   && this.state.sequence
+    //   && this.state.validLength === this.state.sequence.length) {
+    //   this.setSequenceAndClose(this.props.focusedBlocks[0], this.state.sequence);
+    // }
+    this.props.uiShowDNAImport(false);
   }
 
-  setSequenceAndClose(blockId, sequence) {
-    this.props.blockSetSequence(blockId, sequence)
-      .then(block => {
-        // close the dialog, focus the block we inserted into and message the user.
-        this.props.uiShowDNAImport(false);
-        this.props.focusBlocks([blockId]);
-        this.props.uiSetGrunt(`Sequence was successfully inserted.`);
-      })
-      .catch(reason => {
-        // show the error dialog
-        this.props.uiShowDNAImport(false);
-        this.props.uiSetGrunt(`There was a problem inserting that DNA: ${reason.toString()}`);
-      });
-  }
+  // setSequenceAndClose(blockId, sequence) {
+  //   this.props.blockSetSequence(blockId, sequence)
+  //     .then(block => {
+  //       // close the dialog, focus the block we inserted into and message the user.
+  //       this.props.uiShowDNAImport(false);
+  //       this.props.focusBlocks([blockId]);
+  //       this.props.uiSetGrunt(`Sequence was successfully inserted.`);
+  //     })
+  //     .catch(reason => {
+  //       // show the error dialog
+  //       this.props.uiShowDNAImport(false);
+  //       this.props.uiSetGrunt(`There was a problem inserting that DNA: ${reason.toString()}`);
+  //     });
+  // }
 
   render() {
     // no render when not open
-    if (!this.props.open || !this.state.haveSequence) {
+    if (!this.props.open) {
       return null;
     }
 
     return (<ModalWindow
       open={this.props.open}
-      title="Add Sequence"
       closeOnClickOutside
       closeModal={(buttonText) => {
         this.props.uiShowDNAImport(false);
       }}
       payload={
           <form className="gd-form importdnaform" onSubmit={this.onSubmit.bind(this)}>
-            <div className="title">Add Sequence</div>
-            <textarea
-              onKeyDown={event => {
-                if (event.key === 'Enter') {
-                  this.onSubmit(event);
-                }
-              }}
-              placeholder="Type or paste DNA sequence data here."
-              rows="10"
-              autoComplete="off"
-              autoFocus
-              maxLength="10000000"
-              spellCheck="false"
-              ref="sequenceTextArea"
-              defaultValue={this.state.sequence}
-              onChange={this.onSequenceChanged.bind(this)}/>
-            <label style={{textAlign: 'right'}}>{`Length: ${this.state.validLength}`}</label>
-            <div className={`error ${!this.state.inputValid ? 'visible' : ''}`}>The sequence is not valid</div>
+            <div className="title">Edit Sequence</div>
+            <SequenceEditor sequence={this.state.sequence}/>
             <div style={{width: '75%', textAlign: 'center'}}>
-              <button type="submit" disabled={!(this.state.inputValid && this.state.validLength)}>Add</button>
+              <button type="submit">Apply</button>
               <button
                 type="button"
                 onClick={() => {
@@ -212,3 +167,10 @@ export default connect(mapStateToProps, {
   blockAddComponent,
   focusBlocks,
 })(DNAImportForm);
+
+/*
+  1           ATGCATGCAT ATGCATGCAT ATGCATGCAT ATGCATGCAT
+  41          ATGCATGCAT ATGCATGCAT ATGCATGCAT ATGCATGCAT
+  81          ATGCATGCAT ATGCATGCAT ATGCATGCAT ATGCATGCAT
+
+ */
