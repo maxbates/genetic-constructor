1e98fae0a229ebad83a5d4da0acf236e9da18265
Maxwell Bates <max.bates@autodesk.com>
2016-09-28 12:14:49 -0700

    copy python files into server bundle

--- tools/copy.js
+++ tools/copy.js
@@ -1,5 +1,6 @@
 import Promise from 'bluebird';
 import { writeFile } from './lib/fs';
+import mkdirp  from 'mkdirp';
 import pkg from '../package.json';
 /**
  * Copies static files such as robots.txt, favicon.ico to the
@@ -7,6 +8,7 @@ import pkg from '../package.json';
  */
 async function copy() {
   const ncp = Promise.promisify(require('ncp'));
+  const glob = Promise.promisify(require('glob'));
 
   await Promise.all([
     //public assets
@@ -21,6 +23,23 @@ async function copy() {
 
     //copy installed extensions
     ncp('server/extensions/node_modules', 'build/node_modules'),
+
+    //copy python scripts used by those extensions, relative to extension root
+    glob('server/extensions/**/*.py', function pythonGlob(err, files) {
+      return Promise.all(files.map(file => {
+        const [ server, extensions, type, name, ...splitPath] = file.split('/');
+
+        const filePath = splitPath.join('/');
+        const fileDir = splitPath.slice(0, -1).join('/');
+        const buildPath = 'build/' + filePath;
+
+        if (!!fileDir) {
+          mkdirp.sync('build/' + fileDir);
+        }
+
+        return ncp(file, buildPath);
+      }));
+    }),
   ]);
 
   await writeFile('./build/package.json', JSON.stringify({
