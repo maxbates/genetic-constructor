4a638bf409b1f82c54ef985bea258588d0f3f2eb
cojofra <cojofra@users.noreply.github.com>
2016-10-07 11:41:51 -0700

    Update importpartscsv.js

--- src/components/importpartscsv/importpartscsv.js
+++ src/components/importpartscsv/importpartscsv.js
@@ -175,7 +175,7 @@ class ImportPartsCSVModal extends Component {
                 }}>Cancel
               </button>
               <div className="link">
-                <span>Format documentation and sample .CSV files can be found here
+                <span>Format documentation and sample .CSV files can be found 
                     <a className="blue-link" href="https://geneticconstructor.readme.io/docs/csv-upload" target="_blank">here</a>
                 </span>
               </div>
