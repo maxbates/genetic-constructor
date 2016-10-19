1879a99fafd7e13cda153787846714f8c1686a9b
duncanmeech <duncanmeech@gmail.com>
2016-10-19 14:22:21 -0700

    fix block delete via context menu test and change fixture for opening block context menu

--- test-e2e/tests/block-delete-context-menu.js
+++ test-e2e/tests/block-delete-context-menu.js
@@ -1,9 +1,4 @@
 var homepageRegister = require('../fixtures/homepage-register');
-var signout = require('../fixtures/signout');
-var signin = require('../fixtures/signin');
-var dragFromTo = require('../fixtures/dragfromto');
-var newProject = require('../fixtures/newproject');
-var newConstruct = require('../fixtures/newconstruct');
 var testProject = require('../fixtures/testproject');
 var openNthBlockContextMenu = require('../fixtures/open-nth-block-contextmenu');
 var clickNthContextMenuItem = require('../fixtures/click-popmenu-nth-item');
@@ -14,21 +9,21 @@ module.exports = {
     size(browser);
 
     // register via fixture
-    var credentials = homepageRegister(browser);
+    homepageRegister(browser);
 
     // now we can go to the project page
     testProject(browser);
 
     // delete block from second construct viewer
-    var blockBounds = openNthBlockContextMenu(browser, '.construct-viewer:nth-of-type(2) .sceneGraph', 0);
+    openNthBlockContextMenu(browser, '.construct-viewer:nth-of-type(2) .sceneGraph', 0);
     clickNthContextMenuItem(browser, 2);
 
     // NOTE: The last item add will be selected. Clicking the first item will group select all blocks
 
-    // expect all blocks to be deleted
+    // expect 5 blocks ( there were 6 )
     browser
       .pause(250)
-      .assert.countelements('[data-nodetype="block"]', 0)
+      .assert.countelements('[data-nodetype="block"]', 5)
       .end();
   }
 };
