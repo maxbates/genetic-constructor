a1b1cee148888e64ac3e4e17dc565e025b2ef36b
Maxwell Bates <max.bates@autodesk.com>
2016-11-15 18:22:13 -0800

    add startdb to start-instance script

--- tools/start-instance.js
+++ tools/start-instance.js
@@ -4,6 +4,7 @@ import setup from './setup';
 import checks from './checks';
 import bundle from './bundle';
 import clean from './clean';
+import startDb from './startDb';
 import copy from './copy';
 
 //This is a short term file which is used to build the client, and run the server once. Meant for short-term production use, getting rid of webpack middleware etc., but still running the server in babel-node.
@@ -12,6 +13,7 @@ async function startInstance() {
   await run(checks);
   await run(clean);
   await run(setup);
+  await run(startDb);
   await run(copy.bind(undefined, { watch: true }));
   await run(bundle);
 
