fcad6dd04b82d9e9059d2502633ef45fc529248d
duncanmeech <duncanmeech@gmail.com>
2016-11-22 11:08:53 -0800

    fix tests related to ribbon and removing extension dialog from user menu in global nav

--- test-e2e/fixtures/signout.js
+++ test-e2e/fixtures/signout.js
@@ -1,13 +1,15 @@
 var signout = function(browser) {
   // NOTE: This only works if currently signed in
   browser
+    .waitForElementPresent('.ribbongrunt-hidden', 5000, 'expected ribbon to go away')
+    .pause(500)
     // click user widget to start sign out
     .waitForElementPresent('div.signed-in', 5000, 'expected user to be signed in')
     .click('div.signed-in')
     // click sign out menu item
     .waitForElementPresent('.menu-item:nth-of-type(3)', 5000, 'expected menu to appear')
     .pause(1000)
-    .click('.menu-item:nth-of-type(4)')
+    .click('.menu-item:nth-of-type(3)')
     .waitForElementPresent('.homepage', 5000, 'expected to be signed out')
 }
 
