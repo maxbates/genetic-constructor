39fcac108ebc14cf1bcc4eea86f7dfeea2a0d840
Maxwell Bates <max.bates@autodesk.com>
2016-12-02 16:21:52 -0800

    add project file migration script, with updating project and pushing to s3

--- bin/migrate/fileMigration.js
+++ bin/migrate/fileMigration.js
@@ -22,18 +22,23 @@ import fs from 'fs';
 import _ from 'lodash';
 import { defaultUser } from '../../server/auth/local';
 
+import { errorDoesNotExist } from '../../server/utils/errors';
 import * as fileSystem from '../../server/data/middleware/fileSystem';
 import * as s3 from '../../server/data/middleware/s3';
 import * as projectFiles from '../../server/data/files/projectFiles';
+import * as projectPersistence from '../../server/data/persistence/projects';
 
 import batchPromises from './batchPromises';
 import { storagePath, projectPath, AUTH_API } from './config';
 
-//if (!s3.useRemote) {
-  //throw new Error('must use S3 - pass s3 credentials to propcess');
-//}
+if (!s3.useRemote) {
+  throw new Error('must use S3 - pass s3 credentials to propcess');
+}
 
-const files = {};
+const extensionName = 'gc-gsl-editor';
+const gslFileName = 'project.gsl';
+
+const files = [];
 
 console.log('checking all projects in ', projectPath);
 
@@ -49,14 +54,14 @@ _.forEach(projects, projectId => {
   const projectFilesPath = path.resolve(projectPath, projectId, 'data', 'files');
   let extensions;
 
+  //get the files in the extension (expect there to only be one extension)
+  //get the extensions in the project
   try {
     extensions = fs.readdirSync(projectFilesPath);
   } catch (err) {
     console.log('no files in project ' + projectId);
     return;
   }
-
-  files[projectId] = {};
 
   _.forEach(extensions, extension => {
     const projectFilesExtensionPath = path.resolve(projectFilesPath, extension);
@@ -69,43 +74,86 @@ _.forEach(projects, projectId => {
       return;
     }
 
-    files[projectId][extension] = fileList;
+    if (fileList.indexOf(gslFileName) >= 0) {
+      const gslFilePath = path.resolve(projectFilesExtensionPath, gslFileName);
+
+      files.push({
+        projectId,
+        extension, //the old extension name (should be namespaced into new extension name)
+        gslPath: gslFilePath,
+      });
+    }
+
+    //expected project.gsl not found
+    console.log('couldnt find expected file for project', projectId, extension, fileList);
   });
 });
 
-console.log(files);
-
-const extensions = _.uniq(..._.map(files, (extensionObj, projectId) => Object.keys(extensionObj)));
-console.log(extensions);
-
 // move project files into s3
 
-/*
-batchPromises(_.map(files, (fileNames, projectId) => () => {
-  console.log('writing files for project ' + projectId);
+batchPromises(_.map(files, (fileObject) => () => {
+  const { gslPath, extension, projectId } = fileObject;
 
-  //we only want to write GSL files... nothing else has made project files
-  const acceptable = ['project.gsl'];
-  const toWrite = _.union(fileNames, acceptable);
-
-  console.log('writing:', toWrite);
-
-  return Promise.all(toWrite.map(fileName => projectFiles.projectFile)
+  return fileSystem.fileRead(gslPath, false)
+    .catch(err => {
+      console.log('error reading file', gslPath);
+      throw err;
+    })
+    .then(fileContents => {
+      return projectFiles.projectFileWrite(projectId, extensionName, gslFileName, fileContents)
+        .then((fileInfo) => {
+          console.log('wrote project file for project', extension, fileInfo);
+          Object.assign(fileObject, fileInfo);
+        })
+        .catch(err => {
+          console.log('error writing file', projectId);
+          throw err;
+        });
+    });
 }))
   .then(() => {
-    console.log('project files migrated');
+    console.log('project files added to s3');
   })
   .catch(err => {
     console.log(err, err.stack);
     throw err;
+  })
+  //now update all the projects so they know about their files
+  .then(() => {
+    return batchPromises(_.map(files, (fileObj) => () => {
+      const { projectId, VersionId } = fileObj;
+
+      return projectPersistence.projectGetManifest(projectId)
+        .then(manifest => {
+          //note - this will overwrite the files section, but that should be fine since only have GSL at this point
+
+          const patch = {
+            files: [{
+              name: gslFileName,
+              namespace: extensionName,
+              version: VersionId,
+            }],
+          };
+
+          const userId = manifest.authors[0];
+
+          return projectPersistence.projectWriteManifest(projectId, patch, userId, false)
+            .catch(err => {
+              console.log('error writing manifest');
+              console.log(err);
+              throw err;
+            });
+        })
+        .catch(err => {
+          //if project doesnt exist, then whatever who cares
+          if (err === errorDoesNotExist) {
+            console.log('project did not exist, ignoring');
+            return;
+          }
+
+          console.log('error updating manifest with fils');
+          console.log(err.stack);
+          throw err;
+        });
+    }));
   });
-*/
-// todo - update the projects so they know about their files - will need to include versioning information on the write
-//should make sure the project atually exists in the database, and should only add once
-
-//todo - check importers and see how they expect these to exist? may just need to copy by file name (maybe handle projectId)
-// move imported files (now, job files)
-// namespace by projectId - will need to check projects' block.source.id ???
-// appropriate namespacing (Genbank, CSV) -> import
-
-
