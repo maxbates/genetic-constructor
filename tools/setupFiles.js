18659d198f188bdd6125327dc7ceeca76c6e0681
Maxwell Bates <max.bates@autodesk.com>
2016-12-05 13:05:02 -0800

    lint fix. e2e test for sequence import waits for save banner

--- tools/setupFiles.js
+++ tools/setupFiles.js
@@ -57,7 +57,7 @@ async function setupFiles() {
         .catch(err => {
           console.log('Error copying EGF sequences, continuing...');
           console.log(err.stack);
-        })
+        });
     });
 }
 
