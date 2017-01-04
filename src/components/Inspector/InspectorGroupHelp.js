4d616244d92253911eb7b7991c5b8390376d1706
duncanmeech <duncanmeech@gmail.com>
2017-01-04 10:30:02 -0800

    changes as per card

--- src/components/Inspector/InspectorGroupHelp.js
+++ src/components/Inspector/InspectorGroupHelp.js
@@ -13,11 +13,23 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import {
+  inspectorSelectTab,
+} from '../../actions/ui';
 
 import '../../styles/InspectorGroupHelp.css';
 
-export default class InspectorGroupHelp extends Component {
+class InspectorGroupHelp extends Component {
+  static propTypes = {
+    inspectorSelectTab: PropTypes.func.isRequired,
+  };
+
+  onBugOrFeedback = (event) => {
+    event.preventDefault();
+    this.props.inspectorSelectTab('Feedback');
+  };
 
   render() {
     return (<div className="InspectorGroupHelp">
@@ -33,21 +45,29 @@ export default class InspectorGroupHelp extends Component {
       <a target="_blank" href="https://geneticconstructor.readme.io/docs/edinburgh-genome-foundry">Edinburgh Genome Foundry</a>
       <br/>
       <div className="Section">Information for Developers</div>
-      <a target="_blank" href="https://geneticconstructor.readme.io/docs/how-to-contribute">Contributing to</a>
+      <a target="_blank" href="https://geneticconstructor.readme.io/docs/how-to-contribute">Contributing to Genetic Constructor</a>
       <a target="_blank" href="https://geneticconstructor.readme.io/docs/about-extensions">Authoring Plugins</a>
       <a target="_blank" href="https://github.com/Autodesk/genetic-constructor">API Documentation</a>
       <br/>
       <div className="Section">Get Support</div>
       <a target="_blank" href="https://geneticconstructor.readme.io/discuss">Community Discussions</a>
-      <a href="#">Report a Bug</a>
-      <a href="#">Give us Feedback</a>
+      <a onClick={this.onBugOrFeedback} href="#">Report a Bug</a>
+      <a onClick={this.onBugOrFeedback} href="#">Give us Feedback</a>
       <br/>
       <div className="Section">Information</div>
-      <a href="#">About Genetic Constructor</a>
-      <a href="#">Terms of Use</a>
-      <a href="#">Privacy Policy</a>
+      <a target="_blank" href="http://bionano.autodesk.com/GeneticDesign/index.html">About Genetic Constructor</a>
+      <a target="_blank" href="http://www.autodesk.com/company/legal-notices-trademarks/terms-of-service-autodesk360-web-services">Terms of Use</a>
+      <a target="_blank" href="http://www.autodesk.com/company/legal-notices-trademarks/privacy-statement">Privacy Policy</a>
       <a target="_blank" href="http://bionano.autodesk.com/">More from Autodesk® BioNano Research</a>
     </div>);
   }
 }
+
+function mapStateToProps(state, props) {
+  return {};
+}
+
+export default connect(mapStateToProps, {
+  inspectorSelectTab,
+})(InspectorGroupHelp);
 
