b8bf7b20cac83b9ff81d5a035055ef7fdaf31e6b
duncanmeech <duncanmeech@gmail.com>
2017-02-02 09:35:38 -0800

    fix palette picker test again with longer delay to remove flakyness

--- test-e2e/tests/palette-picker.js
+++ test-e2e/tests/palette-picker.js
@@ -1,6 +1,6 @@
 var homepageRegister = require('../fixtures/homepage-register');
 var size = require('../fixtures/size');
-var openInventoryPanel = require('../fixtures/open-inventory-panel');
+var openInspectorPanel = require('../fixtures/open-inspector-panel');
 var testProject = require('../fixtures/testproject');
 
 module.exports = {
@@ -8,18 +8,18 @@ module.exports = {
     size(browser);
     homepageRegister(browser);
     testProject(browser);
-    openInventoryPanel(browser, 'Projects');
+    openInspectorPanel(browser, 'Information');
     browser
       .pause(3000)
       // test palette picker on project
-      .click('.ProjectHeader .title')
+      .click('.ProjectHeader .title .text')
       .waitForElementPresent('.expando[data-expando*="Color Palette"]', 1000, 'expected color palette picker expando')
       .click('.expando[data-expando*="Color Palette"]')
-      .waitForElementPresent('.palette-picker-content > .color-picker > .color', 1000, 'expected a color chip')
+      .waitForElementPresent('.palette-picker-content > .color-picker > .color', 5000, 'expected a color chip')
       .assert.countelements('.palette-picker-content > .color-picker > .color', 16)
     // test palette picker on construct
     browser
-      .click('.construct-viewer .title-and-toolbar .title')
+      .click('.construct-viewer .title')
       .waitForElementPresent('.expando[data-expando*="Color Palette"]', 1000, 'expected color palette picker expando')
       // should already be open since it state is synched to the project palette picker
       .waitForElementPresent('.palette-picker-content > .color-picker > .color', 1000, 'expected a color chip')
