9e2c2eb67a7557641983423f41249719fdd06abb
duncanmeech <duncanmeech@gmail.com>
2017-02-02 08:20:21 -0800

    fix palette picker test

--- test-e2e/tests/palette-picker.js
+++ test-e2e/tests/palette-picker.js
@@ -12,14 +12,14 @@ module.exports = {
     browser
       .pause(3000)
       // test palette picker on project
-      .click('.ProjectHeader')
+      .click('.ProjectHeader .title')
       .waitForElementPresent('.expando[data-expando*="Color Palette"]', 1000, 'expected color palette picker expando')
       .click('.expando[data-expando*="Color Palette"]')
       .waitForElementPresent('.palette-picker-content > .color-picker > .color', 1000, 'expected a color chip')
       .assert.countelements('.palette-picker-content > .color-picker > .color', 16)
     // test palette picker on construct
     browser
-      .click('.node[data-nodetype="construct-title"]')
+      .click('.construct-viewer .title-and-toolbar .title')
       .waitForElementPresent('.expando[data-expando*="Color Palette"]', 1000, 'expected color palette picker expando')
       // should already be open since it state is synched to the project palette picker
       .waitForElementPresent('.palette-picker-content > .color-picker > .color', 1000, 'expected a color chip')
