46f67dce9809486e265714deed9c54b0e84ed753
duncanmeech <duncanmeech@gmail.com>
2017-02-13 13:27:53 -0800

    fix delete project test

--- test-e2e/tests/deleteproject.js
+++ test-e2e/tests/deleteproject.js
@@ -12,7 +12,12 @@ module.exports = {
     openInventoryPanel(browser, 'Projects')
     browser
       .waitForElementPresent('.inventory-project-tree .expando', 5000, 'expected project tree with one project')
-      .assert.countelements('.inventory-project-tree .expando', 1);
+      .assert.countelements('.inventory-project-tree .expando', 1)
+      .assert.countelements('[data-testid^="project"]', 1)
+      .pause(1000)
+      .click('[data-testid="NewProjectButton"')
+      .pause(3000)
+      .assert.countelements('[data-testid^="project"]', 2);
 
     rightClickAt(browser, '.inventory-project-tree .expando', 40, 10);
     clickMenuNthItem(browser, 8);
@@ -20,9 +25,11 @@ module.exports = {
     browser
       // wait for confirmation dialog and accept
       .waitForElementPresent('.ok-cancel-form button[type="submit"]', 5000, 'expected confirmation dialog')
-      .pause(1000)
+      .pause(3000)
       .click('.ok-cancel-form button[type="submit"]')
-      .waitForElementNotPresent('.inventory-project-tree .expando', 5000, 'expected no projects')
+      .waitForElementNotPresent('.ok-cancel-form button[type="submit"]', 5000, 'expected confirmation dialog to go away')
+      .pause(3000)
+      .assert.countelements('[data-testid^="project"]', 1)
       .end();
 
   }
