da94e5aed2b7875078a9b7fcd4700c96efe5adbb
duncanmeech <duncanmeech@gmail.com>
2017-02-28 12:54:51 -0800

    add fixture for selecting specific blocks and fix tests

--- test-e2e/fixtures/open-nth-block-contextmenu.js
+++ test-e2e/fixtures/open-nth-block-contextmenu.js
@@ -1,26 +1,23 @@
+const selectNthBlock = require('./select-nth-block');
 /**
  * get the nth block, within the given selector, bounds in document space
  */
-module.exports = function (browser, srcSelector, blockIndex) {
+module.exports = function (browser, constructSelector, blockIndex) {
+  selectNthBlock(browser, constructSelector, blockIndex);
 
-  var b;
   // generate mouse move events on body from source to destination
-  browser.execute(function(srcSelector, blockIndex) {
-
-    var src = document.querySelector(srcSelector);
-    var blocks = src.querySelectorAll('[data-nodetype="block"]');
-    var block = blocks[blockIndex];
-    var bounds = block.getBoundingClientRect();
-    return {left: bounds.left , top: bounds.top, width: bounds.width, height: bounds.height};
-
-  }, [srcSelector, blockIndex], function(result) {
-    b = result.value;
+  browser.execute((constructSelector, blockIndex) => {
+    const src = document.querySelector(constructSelector);
+    const blocks = src.querySelectorAll('[data-nodetype="block"]');
+    const block = blocks[blockIndex];
+    const b = block.getBoundingClientRect();
+    return { left: b.left, top: b.top, right: b.right, bottom: b.bottom };
+  }, [constructSelector, blockIndex], (result) => {
+    const b = result.value;
 
     browser
-      .moveToElement('body', b.left + b.width - 8, b.top + 15)
-      .mouseButtonClick('left')
-      .pause(500)
+      .moveToElement('body', b.right - 10, b.bottom - 10)
       .mouseButtonClick('right')
       .waitForElementPresent('.menu-overlay .menu-overlay-menu', 5000, 'expected an open menu');
   });
-}
+};
