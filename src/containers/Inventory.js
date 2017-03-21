eea2b355a6a128ad71d3d9efccc86a36352dcddb
Maxwell Bates <max.bates@autodesk.com>
2017-03-21 10:22:00 -0700

    remove templates tab

--- src/containers/Inventory.js
+++ src/containers/Inventory.js
@@ -50,10 +50,12 @@ export class Inventory extends Component {
       title: 'Projects',
       dragTarget: true,
     },
+    /*
     Templates: {
       type: 'templates',
       title: 'Templates',
     },
+    */
     Commons: {
       type: 'commons',
       title: 'Commons',
