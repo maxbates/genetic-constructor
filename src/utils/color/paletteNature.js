98f60c9b3fe25b23b14efada38de9c20948f0327
Joe Lachoff <jlachoff@users.noreply.github.com>
2017-03-27 12:08:37 -0700

    Update paletteNature.js

--- src/utils/color/paletteNature.js
+++ src/utils/color/paletteNature.js
@@ -13,21 +13,43 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+
+//experimental palette PASTEL
+
 export default [
-  { hex: '#418870', name: '' },
-  { hex: '#88c2b3', name: '' },
-  { hex: '#44848c', name: '' },
-  { hex: '#8ac2c9', name: '' },
-  { hex: '#81903a', name: '' },
-  { hex: '#c0cf5a', name: '' },
-  { hex: '#7c9e77', name: '' },
-  { hex: '#95be90', name: '' },
-  { hex: '#fae050', name: '' },
-  { hex: '#f7f1a6', name: '' },
-  { hex: '#b5ad9f', name: '' },
-  { hex: '#ded8cb', name: '' },
-  { hex: '#df7b37', name: '' },
-  { hex: '#ecb685', name: '' },
-  { hex: '#bc4f56', name: '' },
-  { hex: '#d6727b', name: '' },
+  { hex: '#D47A86', name: '' },
+  { hex: '#4382B8', name: '' },
+  { hex: '#D54084', name: '' },
+  { hex: '#376CBF', name: '' },
+  { hex: '#EB9E89', name: '' },
+  { hex: '#74A496', name: '' },
+  { hex: '#D1EB89', name: '' },
+  { hex: '#A9D171', name: '' },
+  { hex: '#587C84', name: '' },
+  { hex: '#275F78', name: '' },
+  { hex: '#899B90', name: '' },
+  { hex: '#BBBA9C', name: '' },
+  { hex: '#814E99', name: '' },
+  { hex: '#8A6CAB', name: '' },
+  { hex: '#908BAC', name: '' },
+  { hex: '#85CFAE', name: '' },
 ];
+
+//export default [
+//  { hex: '#418870', name: '' },
+//  { hex: '#88c2b3', name: '' },
+//  { hex: '#44848c', name: '' },
+//  { hex: '#8ac2c9', name: '' },
+//  { hex: '#81903a', name: '' },
+//  { hex: '#c0cf5a', name: '' },
+//  { hex: '#7c9e77', name: '' },
+//  { hex: '#95be90', name: '' },
+//  { hex: '#fae050', name: '' },
+//  { hex: '#f7f1a6', name: '' },
+//  { hex: '#b5ad9f', name: '' },
+//  { hex: '#ded8cb', name: '' },
+//  { hex: '#df7b37', name: '' },
+//  { hex: '#ecb685', name: '' },
+//  { hex: '#bc4f56', name: '' },
+//  { hex: '#d6727b', name: '' },
+//];
