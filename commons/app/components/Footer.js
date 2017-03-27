72841d188d43faa1c800cc43df4228ec583b57b9
Maxwell Bates <max.bates@autodesk.com>
2017-03-27 13:22:59 -0700

    footer

--- commons/app/components/Footer.js
+++ commons/app/components/Footer.js
@@ -15,8 +15,84 @@
  */
 import React from 'react';
 
+import '../styles/Footer.css';
+
 export default function Footer() {
   return (
-    <div className="Footer"></div>
+    <div className="Footer">
+      <div className="Footer-row Footer-links">
+        <div className="Footer-column">
+          <h4>Genetic Constructor</h4>
+
+          <a
+            href="https://geneticconstructor.readme.io/docs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Documentation</a>
+          <a href="homepage/register">Create your account</a>
+          <a href="homepage/signin">Sign In</a>
+          <a
+            href="http://www.autodesk.com/company/legal-notices-trademarks/privacy-statement"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Privacy</a>
+        </div>
+
+        <div className="Footer-column">
+          <h4>Products</h4>
+
+          <a
+            href="https://molviewer.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Molecule Viewer</a>
+          <a
+            href="http://molsim.bionano.autodesk.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Molecular Simulation Toolkit</a>
+          <a
+            href="http://bionano.autodesk.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >The BioNano Team</a>
+          <a
+            href="http://www.autodesk.com/company"
+            target="_blank"
+            rel="noopener noreferrer"
+          >About Autodesk</a>
+        </div>
+
+        <div className="Footer-column">
+          <h4>Contact Us</h4>
+
+          <a
+            href="https://www.facebook.com/autodeskbionano"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Facebook</a>
+          <a
+            href="https://twitter.com/adskBioNano"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Twitter</a>
+          <a
+            href="https://www.youtube.com/channel/UC19GH6wqbQMnOe2fF4DZlZA"
+            target="_blank"
+            rel="noopener noreferrer"
+          >YouTube</a>
+          <a href="mailto:geneticconstructor@autodesk.com">Email us</a>
+        </div>
+      </div>
+
+      <div className="Footer-row">
+        <img
+          src="/landing/assets/images/Autodesk.svg"
+          width="150"
+          alt="Autodesk Logo"
+        />
+        <p style={{ paddingTop: '1rem' }}>&copy; 2017</p>
+      </div>
+    </div>
   );
 }
