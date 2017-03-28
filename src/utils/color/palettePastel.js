90bbb49f9fe65b62a02b7b3901162199f068b05f
Joe Lachoff <jlachoff@users.noreply.github.com>
2017-03-27 20:28:44 -0700

    Update palettePastel.js

--- src/utils/color/palettePastel.js
+++ src/utils/color/palettePastel.js
@@ -34,22 +34,3 @@ export default [
   { hex: '#908BAC', name: '' },
   { hex: '#85CFAE', name: '' },
 ];
-
-//export default [
-//  { hex: '#fbedcc', name: '' },
-//  { hex: '#f3e39e', name: '' },
-//  { hex: '#fbebd3', name: '' },
-//  { hex: '#f8c6a4', name: '' },
-//  { hex: '#f2dbf3', name: '' },
-//  { hex: '#fbb8d6', name: '' },
-//  { hex: '#ead1fb', name: '' },
-//  { hex: '#e7aaff', name: '' },
-//  { hex: '#bde8f3', name: '' },
-//  { hex: '#9ae3f8', name: '' },
-//  { hex: '#bff8e3', name: '' },
-//  { hex: '#8cf6cb', name: '' },
-//  { hex: '#ddd7bf', name: '' },
-//  { hex: '#c4bc99', name: '' },
-//  { hex: '#d6d5d4', name: '' },
-//  { hex: '#aeaca9', name: '' },
-//];
