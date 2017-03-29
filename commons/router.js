df4abcc2d9f9f669617be828d8874d5f46dea17f
Maxwell Bates <max.bates@autodesk.com>
2017-03-29 13:25:42 -0700

    add HTML and error page component

--- commons/router.js
+++ commons/router.js
@@ -16,47 +16,21 @@
 import Express from 'express';
 import React from 'react';
 import _ from 'lodash';
-import { renderToString } from 'react-dom/server';
+import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import commonsReducer from './app/reducers';
-import routes from './app/routes';
+import PrettyError from 'pretty-error';
 
 import { projectVersionByUUID } from '../server/data/persistence/projectVersions';
 import * as commons from '../server/data/persistence/commons';
 
+import commonsReducer from './app/reducers';
+import routes from './app/routes';
+import Html from './app/components/Html';
+import ErrorPage from './app/components/ErrorPage';
+
 const router = Express.Router(); //eslint-disable-line new-cap
-
-function renderFullPage(html, preloadedState) {
-  const prod = process.env.NODE_ENV === 'production';
-  const stateString = JSON.stringify(preloadedState).replace(/</g, '\\u003c');
-  const cssLink = '<link rel="stylesheet" href="/static/commons.css"></script>';
-
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Genetic Constructor - Commons</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Genetic Constructor Commons hosts published projects and constructs from users.">
-        <meta name="author" content="Autodesk Life Sciences">
-        <meta name="keywords" content="genetic design software, genetic design tool, dna sequence editor, molecular design software, promoter library, CAD software for biology">
-        <link rel="canonical" href="https://geneticconstructor.bionano.autodesk.com">
-        ${cssLink}
-      </head>
-      <body>
-        <div id="root">${prod ? html : ''}</div>
-        <script>
-          window.__PRELOADED_STATE__ = ${stateString}
-        </script>
-       <script src="/static/commons.js"></script>
-      </body>
-    </html>
-    `;
-}
 
 //todo - update react router: https://reacttraining.com/react-router/web/guides/server-rendering
 
@@ -70,44 +44,75 @@ function renderFullPage(html, preloadedState) {
 // https://github.com/gaearon/react-hot-loader/tree/master/docs#migration-to-30
 
 async function handleRender(req, res, next) {
-  const snapshots = await commons.commonsQuery();
+  try {
+    const snapshots = await commons.commonsQuery();
 
-  const fetchedProjects = await Promise.all(
-    snapshots.map(({ projectUUID }) => projectVersionByUUID(projectUUID)),
-  );
+    const fetchedProjects = await Promise.all(
+      snapshots.map(({ projectUUID }) => projectVersionByUUID(projectUUID)),
+    );
 
-  const projects = _.keyBy(fetchedProjects, proj => proj.project.id);
+    const projects = _.keyBy(fetchedProjects, proj => proj.project.id);
 
-  match({ routes, location: req.originalUrl }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    } else if (renderProps) {
-      // Create a new Redux store instance
-      const store = createStore(commonsReducer, { projects, snapshots });
+    match({ routes, location: req.originalUrl }, (error, redirectLocation, renderProps) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else if (redirectLocation) {
+        res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      } else if (renderProps) {
+        // Create a new Redux store instance
+        const store = createStore(commonsReducer, { projects, snapshots });
 
-      // Grab the initial state from our Redux store
-      const preloadedState = store.getState();
+        // Grab the initial state from our Redux store
+        const preloadedState = store.getState();
 
-      // Render the component to a string
-      const html = renderToString(
-        <Provider store={store}>
-          <RouterContext {...renderProps}>
-            {routes}
-          </RouterContext>
-        </Provider>,
-      );
+        // Render the component to a string
+        const appHtml = renderToString(
+          <Provider store={store}>
+            <RouterContext {...renderProps}>
+              {routes}
+            </RouterContext>
+          </Provider>,
+        );
 
-      // Send the rendered page back to the client
-      res.send(renderFullPage(html, preloadedState));
-    } else {
-      //todo - move 404 rendering into the app
-      res.status(404).send('Not found');
-    }
-  });
+        const html = renderToStaticMarkup(
+          <Html state={preloadedState}>
+          {appHtml}
+          </Html>
+        );
+        res.send(`<!doctype html>${html}`);
+      } else {
+        //todo - move 404 rendering into the app
+        res.status(404).send('Not found');
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 router.route('*').get(handleRender);
+
+//
+// Error handling
+// -----------------------------------------------------------------------------
+const pe = new PrettyError();
+pe.skipNodeFiles();
+pe.skipPackage('express');
+
+router.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  console.log(pe.render(err)); // eslint-disable-line no-console
+
+  const html = renderToStaticMarkup(
+    <Html
+      title="Internal Server Error"
+      description={err.message}
+      scripts={[]}
+    >
+      {renderToString(<ErrorPage error={err} />)}
+    </Html>,
+  );
+  res.status(err.status || 500);
+  res.send(`<!doctype html>${html}`);
+});
 
 export default router;
